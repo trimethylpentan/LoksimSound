9d045bfe987101f9d5d1e3404ff7974a0202f17f
trimethylpentan <pascal.0399@yahoo.de>
2019-06-17 21:31:56 +0200

    soloud eingebunden

--- LoksimSound/ComplexLoopSound.cpp
+++ LoksimSound/ComplexLoopSound.cpp
@@ -1,5 +1,17 @@
 #include "ComplexLoopSound.h"
 
+#include "soloud.h";
+#include "soloud_wav.h";
+#include <iostream>
+
 void ComplexLoopSound::playSound(string soundFilename)
 {
+	std::cout << "test"; 
+	SoLoud::Soloud soloud;
+	soloud.init();
+
+	SoLoud::Wav wav;
+	wav.load(soundFilename.c_str());
+
+	soloud.play(wav);
 }
